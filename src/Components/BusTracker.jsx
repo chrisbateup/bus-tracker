5cd4f6d54bb1a5f4d6371fb5d85d0d970dacf61e
Chris Bateup <github@chrisbateup.com>
2018-08-22 14:21:06 +1000

    Add Snackbar

--- src/Components/BusTracker.jsx
+++ src/Components/BusTracker.jsx
@@ -6,6 +6,9 @@ import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import Snackbar from '@material-ui/core/Snackbar'
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -56,9 +59,6 @@ const styles = theme => ({
       maxWidth: 600,
       margin: '0 auto',
       padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
-    },
-    card: {
-        minHeight: '420px',
     },
     cardHeader: {
       backgroundColor: theme.palette.grey[300],
@@ -114,6 +114,11 @@ const styles = theme => ({
   ];
 
 class BusTracker extends Component {
+    state = {
+        snackbarOpen: false,
+        snackbarText: ''
+      };
+
     render() {
         const { classes } = this.props;
 
@@ -127,16 +132,17 @@ class BusTracker extends Component {
             Which bus are you driving today?
           </Typography>
           <Typography variant="title" align="center" color="textSecondary" component="p">
-            {/* All submissions are completely anonymous and are only used to
-            measure data over time. */}
+            Read the options below and click the colour that matches your day!
+            All input is completely anonymous.
           </Typography>
         </div>
-{/* End hero unit */}
-<Grid container spacing={40} alignItems="flex-end">
+        {/* End hero unit */}
+
+        <Grid container spacing={40} alignItems="flex-end">
           {tiers.map(tier => (
             // Enterprise card is full width at sm breakpoint
             <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-              <Card className={classes.card}>
+              <Card>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
@@ -150,9 +156,9 @@ class BusTracker extends Component {
                   </div>
                   {tier.description.map(line => (
                     <Typography variant="subheading" align="center" key={line}>
-                    {line.split('\n').map((item, key) => {
-  return <span key={key}>{item}<br/></span>
-})}<br />
+                      {line.split('\n').map((item, key) => {
+                        return <span key={key}>{item}<br/></span>
+                      })}<br />
                     </Typography>
                   ))}
                 </CardContent>
@@ -164,8 +170,33 @@ class BusTracker extends Component {
               </Card>
             </Grid>
           ))}
-</Grid>
+        </Grid>
                 </main>
+
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={this.state.snackbarOpen}
+          autoHideDuration={6000}
+          onClose={this.handleSnackbarClose}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span id="message-id">{this.state.snackbarText}</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.snackbarClose}
+              onClick={this.handleSnackbarClose}
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
             </React.Fragment>
         );
     }
@@ -181,12 +212,36 @@ class BusTracker extends Component {
               body: JSON.stringify({colour:busColour})
             });
             await rawResponse;
+
+            if (rawResponse.ok) {
+                this.showSavedResponse();
+            } else {
+                this.showFailedResponse(rawResponse.status, rawResponse.statusText);
+            }
           })();
     }
+
+    handleSnackbarClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        this.setState({ snackbarOpen: false });
+      };
+
+      showSavedResponse = () => {
+          this.setState({snackbarText: 'Thank you! Your selection has been saved.'})
+        this.setState({snackbarOpen: true})
+      }
+
+      showFailedResponse = (errorStatus, errorMessage) => {
+        this.setState({snackbarText: errorStatus + ' ' + errorMessage})
+        this.setState({snackbarOpen: true})
+      }
 }
 
 BusTracker.propTypes = {
     classes: PropTypes.object.isRequired,
   };
   
-  export default withStyles(styles)(BusTracker);
+export default withStyles(styles)(BusTracker);
